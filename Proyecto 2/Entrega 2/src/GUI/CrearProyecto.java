84151ec2130f1d17a62080ea06e88f710d3f6b7a
luna-as18 <m.lunav@uniandes.edu.co>
2022-04-27 17:34:26 -0500

    Swing design

---
+++ Proyecto 2/Entrega 2/src/GUI/CrearProyecto.java
@@ -0,0 +1,55 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class CrearProyecto extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtS;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CrearProyecto frame = new CrearProyecto();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CrearProyecto() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel = new JLabel("Nombre Proyecto: ");
+		panel.add(lblNewLabel);
+		
+		txtS = new JTextField();
+		panel.add(txtS);
+		txtS.setColumns(10);
+	}
+
+}
